e6d0569a9289a0db7e819211b042799e7f632c3a
twagirumukiza <38731174+twagirumukiza@users.noreply.github.com>
2026-07-31 04:07:51 +0200

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -12,12 +12,12 @@
    5. Publier les règles de sécurité fournies dans firebase-rules.json
    ========================================================= */
 
-const FIREBASE_CONFIG = {
-  apiKey: "REMPLACE_MOI",
-  authDomain: "REMPLACE_MOI.firebaseapp.com",
-  databaseURL: "https://REMPLACE_MOI-default-rtdb.firebaseio.com",
-  projectId: "REMPLACE_MOI",
-  storageBucket: "REMPLACE_MOI.appspot.com",
-  messagingSenderId: "REMPLACE_MOI",
-  appId: "REMPLACE_MOI"
+window.FOCUS_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBG6oid29bMq8GVvBkNvPtSDZTRO5K09uk",
+  authDomain: "focus-game-1c7ee.firebaseapp.com",
+  databaseURL: "https://focus-game-1c7ee-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "focus-game-1c7ee",
+  storageBucket: "focus-game-1c7ee.firebasestorage.app",
+  messagingSenderId: "856695121197",
+  appId: "1:856695121197:web:cfc0d876ba9d1885499fa4"
 };
